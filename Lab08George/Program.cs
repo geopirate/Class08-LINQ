2e08021dc839c384e5381e5d0e735bec0b40f880
George Gaylor <georgegaylor83@outlook.com>
2017-09-22 01:40:51 -0700

    Finished Linqs

--- Lab08George/Program.cs
+++ Lab08George/Program.cs
@@ -9,17 +9,59 @@ namespace Lab08George
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the neighborhood!");
+            Console.WriteLine();
+            // I use the JSON reader class to read from the file and return the object I need
             JSONReader reader = new JSONReader();
             RootObject ourHoods =  reader.ViewWords();
 
-            //Output all of the neighborhoods in this data list
-            /*IEnumerable<RootObject> allQuery = from features in ourHoods.features.
-                           select features;*/
+            // Output all of the neighborhoods in this data list
+            // orderby puts all the empties at the top
+            var allQuery = from features in ourHoods.features
+                           select features;
+            Console.WriteLine("Output all of the neighborhoods in this data list");
+            Console.WriteLine();
+            foreach (Feature hood in allQuery)
+            {
+                Console.Write($"{hood.properties.neighborhood}  ");
+            }
 
-            //Filter out all the neighborhoods that do not have any names
-            //Remove the Duplicates
-            //Rewrite the queries from above, and consolidate all into one single query.
+            // Filter out all the neighborhoods that do not have any names
+            // Rewrite at least one of these questions only using a LINQ query(without lambda statement)
+            // no lambda version of the query directly below
+            /*var removeQuery = from features in ourHoods.features
+                              where features.properties.neighborhood != ""
+                              orderby features.properties.neighborhood
+                              select features;*/
 
+            var removeQuery = ourHoods.features.Where(x => x.properties.neighborhood != "");
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("Filter out all the neighborhoods that do not have any names");
+            Console.WriteLine();
+            foreach (Feature hood in removeQuery)
+            {
+                Console.Write($"{hood.properties.neighborhood}  ");
+            }
+
+            // Remove the Duplicates
+            // the empty is between east village and tribeca
+            var duplicateQuery = ourHoods.features.GroupBy(x => x.properties.neighborhood).Select(x => x.First());
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("Remove the Duplicates");
+            Console.WriteLine();
+            foreach (Feature hood in duplicateQuery)
+            {
+                Console.Write($"{hood.properties.neighborhood}  ");
+            }
+
+            // Rewrite the queries from above, and consolidate all into one single query.
+            var combinedQuery = ourHoods.features.Where(x => x.properties.neighborhood != "").GroupBy(x => x.properties.neighborhood).Select(x => x.First());
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("All together now!");
+            Console.WriteLine();
+            foreach (Feature hood in combinedQuery)
+            {
+                Console.Write($"{hood.properties.neighborhood}  ");
+            }
             Console.Read();
         }
     }
@@ -35,9 +77,9 @@ namespace Lab08George
  * * You will need to break up each section of the JSON file up into different classes, use your resources - ask the TA's if your stuck.
  * 
  * Each query builds off of the next.
- * Output all of the neighborhoods in this data list
- * Filter out all the neighborhoods that do not have any names
- * Remove the Duplicates
- * Rewrite the queries from above, and consolidate all into one single query.
- * Rewrite at least one of these questions only using a LINQ query(without lambda statement)
+ * * Output all of the neighborhoods in this data list
+ * * Filter out all the neighborhoods that do not have any names
+ * * Remove the Duplicates
+ * * Rewrite the queries from above, and consolidate all into one single query.
+ * * Rewrite at least one of these questions only using a LINQ query(without lambda statement)
  */
---
+++ Lab08George/readme.md
@@ -0,0 +1,9 @@
+﻿How long did it take you to complete this assignment?
+	About 5 hours
+What did you struggle with? Why? How did you solve?
+	I spent about 3.5 hours going down the wrong path trying to pull the data from the JSON file before figuring out a much easier solution.
+	I also struggled with getting the LINQ queries to work against my collection initially, but I read the LINQ docs and got a little inspiration from stack overflow
+What did you learn during this assignment?
+	I learned a lot about manipulating JSON and other non-database data stores in .NET
+What resources did you utilize for this assignment?
+	I used the Newton documentation primarily and trial and error as well as the demo from class, and a little stack overflow
